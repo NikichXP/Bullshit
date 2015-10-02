e9847204f0ab3587f25f1e911fefdbeca891f731
Ruzhevsky Nikita <nikichx2@gmail.com>
2015-10-03 00:10:07 +0300

    Hidden token. P.S. - token renewed

--- VkMessenger/src/Core.java
+++ VkMessenger/src/Core.java
@@ -1,13 +1,19 @@
-/**
- * Created by nikichxp on 01.10.15.
- */
+import java.nio.file.*;
+
 public class Core {
 
-    public final static String tokenId = "0a85e81792e27469f520be72666e37daf47a00b3109074b710cb619626bece4cdf53e68420dc757a766f6";
+    public static String tokenId = null;
     public final static int userId = 9867491;
 
     public static void main (String [] args) {
+        try {
+            tokenId = new String(Files.readAllBytes(Paths.get("D:/.vk")));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         UserAPI get = new UserAPI () ;
         System.out.println(get.getUser(userId));
+
     }
 }
